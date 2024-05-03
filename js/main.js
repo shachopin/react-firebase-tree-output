6219dbc24a228cc243b007fecc8a590097a53503
Glitch (react-firebase-tree-output) <none>
2024-05-03 07:10:26 +0000

    😜🌭 Updated with Glitch

--- js/main.js
+++ js/main.js
@@ -12,42 +12,42 @@
 
     paths: {
       knockout:
-        "https://20191221-ojet-staging.netlify.com/js/libs/knockout/knockout-3.5.0.debug",
+        "https://20191221-ojet-staging.netlify.app/js/libs/knockout/knockout-3.5.0.debug",
       jquery:
-        "https://20191221-ojet-staging.netlify.com/js/libs/jquery/jquery-3.4.1",
+        "https://20191221-ojet-staging.netlify.app/js/libs/jquery/jquery-3.4.1",
       "jqueryui-amd":
-        "https://20191221-ojet-staging.netlify.com/js/libs/jquery/jqueryui-amd-1.12.1",
+        "https://20191221-ojet-staging.netlify.app/js/libs/jquery/jqueryui-amd-1.12.1",
       promise:
-        "https://20191221-ojet-staging.netlify.com/js/libs/es6-promise/es6-promise",
+        "https://20191221-ojet-staging.netlify.app/js/libs/es6-promise/es6-promise",
       hammerjs:
-        "https://20191221-ojet-staging.netlify.com/js/libs/hammer/hammer-2.0.8",
+        "https://20191221-ojet-staging.netlify.app/js/libs/hammer/hammer-2.0.8",
       ojdnd:
-        "https://20191221-ojet-staging.netlify.com/js/libs/dnd-polyfill/dnd-polyfill-1.0.1",
+        "https://20191221-ojet-staging.netlify.app/js/libs/dnd-polyfill/dnd-polyfill-1.0.1",
       ojs:
-        "https://20191221-ojet-staging.netlify.com/js/libs/oj/v8.0.0/debug" +
+        "https://20191221-ojet-staging.netlify.app/js/libs/oj/v8.0.0/debug" +
         (_ojNeedsES5 ? "_es5" : ""),
       ojL10n:
-        "https://20191221-ojet-staging.netlify.com/js/libs/oj/v8.0.0/ojL10n",
+        "https://20191221-ojet-staging.netlify.app/js/libs/oj/v8.0.0/ojL10n",
       ojtranslations:
-        "https://20191221-ojet-staging.netlify.com/js/libs/oj/v8.0.0/resources",
+        "https://20191221-ojet-staging.netlify.app/js/libs/oj/v8.0.0/resources",
       persist:
-        "https://20191221-ojet-staging.netlify.com/js/libs/persist/debug",
-      text: "https://20191221-ojet-staging.netlify.com/js/libs/require/text",
+        "https://20191221-ojet-staging.netlify.app/js/libs/persist/debug",
+      text: "https://20191221-ojet-staging.netlify.app/js/libs/require/text",
       signals:
-        "https://20191221-ojet-staging.netlify.com/js/libs/js-signals/signals",
-      touchr: "https://20191221-ojet-staging.netlify.com/js/libs/touchr/touchr",
+        "https://20191221-ojet-staging.netlify.app/js/libs/js-signals/signals",
+      touchr: "https://20191221-ojet-staging.netlify.app/js/libs/touchr/touchr",
       "regenerator-runtime":
-        "https://20191221-ojet-staging.netlify.com/js/libs/regenerator-runtime/runtime",
-      corejs: "https://20191221-ojet-staging.netlify.com/js/libs/corejs/shim",
+        "https://20191221-ojet-staging.netlify.app/js/libs/regenerator-runtime/runtime",
+      corejs: "https://20191221-ojet-staging.netlify.app/js/libs/corejs/shim",
       customElements:
-        "https://20191221-ojet-staging.netlify.com/js/libs/webcomponents/custom-elements.min",
+        "https://20191221-ojet-staging.netlify.app/js/libs/webcomponents/custom-elements.min",
       proj4:
-        "https://20191221-ojet-staging.netlify.com/js/libs/proj4js/dist/proj4-src",
-      css: "https://20191221-ojet-staging.netlify.com/js/libs/require-css/css",
+        "https://20191221-ojet-staging.netlify.app/js/libs/proj4js/dist/proj4-src",
+      css: "https://20191221-ojet-staging.netlify.app/js/libs/require-css/css",
       "css-builder":
-        "https://20191221-ojet-staging.netlify.com/js/libs/require-css/css-builder",
+        "https://20191221-ojet-staging.netlify.app/js/libs/require-css/css-builder",
       normalize:
-        "https://20191221-ojet-staging.netlify.com/js/libs/require-css/normalize",
+        "https://20191221-ojet-staging.netlify.app/js/libs/require-css/normalize",
     },
   });
 })();
